6b2d296e4a24f975aeed98039784cc9ffd54f2ba
Kyluxx <rizkypratamaikok@gmail.com>
2025-07-03 04:06:07 +0700

    Idk2

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ const client = bedrock.createClient({
   port: 19132,
   version: '1.21.90',
   username: 'EnrickoED',
-  offline: false
+  offline: false,
 })
 
 client.on('start_game', () => {
@@ -214,6 +214,7 @@ const listword = [
   'chicken',
   'dolphin',
   'minecraft',
+  'wither',
 ]
 
 const reslist = ["gg","ez","nice","lol","lmao","wkwk","asik","mantap","gaskeun","ngantuk","kocak","hebat","op","legend","cringe","bruh","serem","cakep","gacor","santuy","pusing","panik","tepar","auto","gila","kasian","susah","rekt","parah","capek","ngide","langsung","lagi","coba","yeay","hore","salah","fix","ampun","ngantuk","tidur","nt","waw","woah","mirip","bener","tipis","gampang","sulit","serius","njir","pede","nabrak","duar","awas","ayo","yuhu","yoi","horee","yaelah","lah","gaje","sedih","kece","ceria","mager","langsungin","tapiii","oke","buset","pecah","meledak","gacorrr","cepat","lambat","nyala","gelap","terang","muncul","ilang","lompat","dor","zebra","nopal","singa","panda","zombie","slime","muter","jauh","deket","healing","skill","ult","meta","update","baru","lama","nerf","buff"]
