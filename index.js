04fd285801642716088ac48875d9f0bca563b79f
Kyluxx <rizkypratamaikok@gmail.com>
2025-07-03 04:07:20 +0700

    Idk4

--- index.js
+++ index.js
@@ -157,10 +157,15 @@ const client = bedrock.createClient({
   version: '1.21.90',
   username: 'EnrickoED',
   offline: false,
+  auth: fs.existsSync('./auth.json') ? require('./auth.json') : undefined
 })
 
 client.on('start_game', () => {
   console.log('Connected!')
+})
+
+client.on('session', (auth) => {
+  fs.writeFileSync('./auth.json', JSON.stringify(auth))
 })
 
 client.on('error', e => console.error('Error:', e))
